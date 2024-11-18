50d5ebac2df7ba31e21f4dd31073de42386a772a
shoaibzaak <shoaibzaki84@gmail.com>
2024-11-19 00:45:24 +0500

    sdf

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -68,29 +68,29 @@ const menuGroups = [
       //   label: "Calendar",
       //   route: "/calendar",
       // },
-      {
-        icon: (
-          <svg
-            className="fill-current"
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
-              fill=""
-            />
-            <path
-              d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
-              fill=""
-            />
-          </svg>
-        ),
-        label: "Profile",
-        route: "/profile",
-      },
+      // {
+      //   icon: (
+      //     <svg
+      //       className="fill-current"
+      //       width="18"
+      //       height="18"
+      //       viewBox="0 0 18 18"
+      //       fill="none"
+      //       xmlns="http://www.w3.org/2000/svg"
+      //     >
+      //       <path
+      //         d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
+      //         fill=""
+      //       />
+      //       <path
+      //         d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
+      //         fill=""
+      //       />
+      //     </svg>
+      //   ),
+      //   label: "Profile",
+      //   route: "/profile",
+      // },
       {
         icon: (
           <svg
@@ -113,6 +113,7 @@ const menuGroups = [
         ),
         label: "UploadCv",
         route: "/upload",
+        visibleForCreator: false,
       },
       {
         icon: (
@@ -184,6 +185,7 @@ const menuGroups = [
         ),
         label: "Jobs",
         route: "/jobs",
+        visibleForCreator: true,
       },
       {
         icon: (
@@ -215,6 +217,7 @@ const menuGroups = [
         ),
         label: "userJobs",
         route: "/userJobs",
+        visibleForCreator: false,
       },
       {
         icon: (
@@ -439,25 +442,41 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   {group.name}
                 </h3>
                 <ul className="mb-6 flex flex-col gap-1.5">
-                  {userCreator
-                    ? group.menuItems
-                        .filter((menuItem) => menuItem.visibleForCreator)
-                        .map((menuItem, menuIndex) => (
+                  {group.menuItems.map((menuItem, menuIndex) => {
+                    const isVisibleForCreator = menuItem.visibleForCreator;
+                    if (userCreator) {
+                      if (
+                        isVisibleForCreator === true ||
+                        typeof isVisibleForCreator === "undefined"
+                      ) {
+                        return (
                           <SidebarItem
                             key={menuIndex}
                             item={menuItem}
                             pageName={pageName}
                             setPageName={setPageName}
                           />
-                        ))
-                    : group.menuItems.map((menuItem, menuIndex) => (
-                        <SidebarItem
-                          key={menuIndex}
-                          item={menuItem}
-                          pageName={pageName}
-                          setPageName={setPageName}
-                        />
-                      ))}
+                        );
+                      }
+                    } else {
+                      if (
+                        isVisibleForCreator === false ||
+                        typeof isVisibleForCreator === "undefined"
+                      ) {
+                        return (
+                          <SidebarItem
+                            key={menuIndex}
+                            item={menuItem}
+                            pageName={pageName}
+                            setPageName={setPageName}
+                          />
+                        );
+                      }
+                    }
+
+                    // Return null if the condition is not met (this ensures that no item is rendered if conditions are not matched)
+                    return null;
+                  })}
                 </ul>
               </div>
             ))}
